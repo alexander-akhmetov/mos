825277e6f1a9394375296395d3a34aa21b732a61
Alexander Akhmetov <me@aleks.sh>
2018-11-03 18:40:23 +0100

    Loading using GRUB

--- src/boot/boot.asm
+++ src/boot/boot.asm
@@ -110,7 +110,6 @@ section .text
 bits 64
 long_mode_start:
 
-    mov rax, 0x2f592f412f4b2f4f  ; OKAY
-    mov qword [0xb8000], rax
-
-    hlt
+    ; call the rust main
+    extern main
+    call main
