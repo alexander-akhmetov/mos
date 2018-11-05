b582027f9435fc2caaf2bca96eb459fbf535e238
Alexander Akhmetov <me@aleks.sh>
2018-11-05 23:37:03 +0100

    upgraded multiboot2 crate

--- src/init/mod.rs
+++ src/init/mod.rs
@@ -2,13 +2,13 @@ use sys;
 
 
 pub unsafe fn hello_world() {
-    kprintln!("--- before system call ---");
+    kprintln!("\n--- before system call ---");
 
     // let's ask the mOS current time
     let timestamp = sys::lib::sys_time();
     // and then send a debug message
     let syslog_call_result = sys::lib::sys_syslog("Hello, mOS!");
-    kprintln!("--- after system call ---");
+    kprintln!("--- after system call ---\n");
 
     // now let's print current time
     kprintln!("current time: {}", timestamp);
