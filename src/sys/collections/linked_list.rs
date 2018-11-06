bd988b4f1b808f3c0b282b81d42efbdb8ea79c20
Alexander Akhmetov <me@aleks.sh>
2018-11-06 21:08:31 +0100

    Linked list implementation

--- src/sys/collections/linked_list.rs
+++ src/sys/collections/linked_list.rs
@@ -1,18 +1,23 @@
-use alloc::rc::Rc;
+use alloc::boxed::Box;
+use alloc::fmt;
+use alloc::string::String;
 use core::marker::PhantomData;
 
 
+type NodeLink<T> = Option<Box<Node<T>>>;
+
+
 #[derive(Debug)]
-struct Node<T> {
+pub struct Node<T> {
     element: T,
-    next: Option<Rc<Node<T>>>,
+    next: NodeLink<T>,
 }
 
 impl<T> Node<T> {
-    pub fn new(element: T) -> Self {
+    pub fn new(element: T, next: NodeLink<T>) -> Self {
         Node {
             element: element,
-            next: None,
+            next: next,
         }
     }
 }
@@ -20,7 +25,7 @@ impl<T> Node<T> {
 
 #[derive(Debug)]
 pub struct LinkedList<T> {
-    first: Option<Rc<Node<T>>>,
+    first: NodeLink<T>,
     phantom: PhantomData<T>,
 }
 
@@ -33,14 +38,49 @@ impl<T> LinkedList<T> {
         }
     }
 
-
-    pub fn push_back(&mut self, _element: T) {
-
-    }
-
     pub fn push_front(&mut self, element: T) {
-        let mut new_node: Node<T> = Node::new(element);
-        // new_node.next = self.first;
-        self.first = Some(Rc::new(new_node));
+        let new_node = Node::new(
+            element,
+            self.first.take(),
+        );
+        self.first = Some(Box::new(new_node));
+    }
+}
+
+
+pub struct Iter<'a, T:'a> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> LinkedList<T> {
+    pub fn iter(&self) -> Iter<T> {
+        Iter { next: self.first.as_ref().map(|node| &**node) }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_ref().map(|node| &**node);
+            &node.element
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn iter() {
+        let mut list = LinkedList::new();
+        list.push_front(1); list.push_front(2); list.push_front(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
     }
 }
