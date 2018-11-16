4e3ec27b7d9781ceebe16dd2a1fc1a5b3ab61086
Alexander Akhmetov <me@aleks.sh>
2018-11-16 19:11:06 +0100

    uname system call

--- src/constants.rs
+++ src/constants.rs
@@ -1,3 +1,6 @@
 use logging;
 
 pub const LOGLEVEL: logging::LogLevels = logging::LogLevels::INFO;
+pub const KERNEL_VERSION: &'static str = env!("CARGO_PKG_VERSION");
+pub const KERNEL_SYSNAME: &'static str = "mos";
+pub const KERNEL_MACHINE: &'static str = "x86_64";
