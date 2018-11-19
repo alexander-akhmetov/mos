5a06f6b99d3fc2d6f1a2ae781ae24def65cb7919
Alexander Akhmetov <me@aleks.sh>
2018-11-19 21:46:28 +0100

    catwelcome

--- lib/userspace/hello_world/src/main.rs
+++ lib/userspace/hello_world/src/main.rs
@@ -23,8 +23,6 @@ fn _start(_argc: isize, _args: *const *const u8) -> isize {
     let time = unsafe { librust::syscall::time() };
     unsafe {
         librust::syscall::debug("# 1 Hello from process");
-        librust::syscall::debug("# 2 Hello from process");
-        librust::syscall::debug("# 3 Hello from process");
     };
     // println!("# Hello, world! time: {} pid: {}", time, pid);
     return 0;
--- src/user/msh/constants.rs
+++ src/user/msh/constants.rs
@@ -1,2 +1,10 @@
 // comamnd shell prompt string
 pub const PROMPT: &str = "[@mos: {path}]$ ";
+
+pub const CAT_WELCOME: &str = "
+            .---------------.
+            |Welcome to mOS!|
+ \\    /\\    '---------------'
+  )  ( ') ----------'
+ (  /  )
+  \\(__)|\n\n";
